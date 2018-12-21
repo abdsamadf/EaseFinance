e9e7e08f926071fe831fa2c51d0be9fa2b1e7880
Samad <abdulsamadf97@gmail.com>
2018-12-22 00:55:00 +0500

    some changes

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from cs50 import SQL
+from src.sql import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
